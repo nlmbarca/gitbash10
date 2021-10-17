26879810b6acbf5e76e97b2b32771667f32aad72
nlmbarca <nicoortuno13@gmail.com>
2021-10-17 15:00:38 -0400

    Latest code 10182021

---
+++ src/test/java/utils/Constants.java
@@ -0,0 +1,8 @@
+package utils;
+
+public class Constants {
+    //WHEN SOMETHING IS FINAL USE CAPS
+    public static final String CONFIGURATION_FILEPATH= System.getProperty("user.dir")+ "/src/test/resources/config/config.properties";
+    public static final int IMPLICIT_WAIT=10;
+
+}
