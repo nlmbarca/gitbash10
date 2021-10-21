8f119e9bc51381912e8ed8727b757ec71c6b6114
nlmbarca <nicoortuno13@gmail.com>
2021-10-20 22:31:19 -0400

    Latest code 10202021

--- src/test/java/utils/Constants.java
+++ src/test/java/utils/Constants.java
@@ -2,7 +2,10 @@ package utils;
 
 public class Constants {
     //WHEN SOMETHING IS FINAL USE CAPS
-    public static final String CONFIGURATION_FILEPATH= System.getProperty("user.dir")+ "/src/test/resources/config/config.properties";
-    public static final int IMPLICIT_WAIT=10;
+    public static final String CONFIGURATION_FILEPATH = System.getProperty("user.dir") + "/src/test/resources/config/config.properties";
+    public static final String TESTDATA_FILEPATH = System.getProperty("user.dir")+ "/src/test/resources/testdata/hrmsdatabatch10.xlsx";
+    public static final int IMPLICIT_WAIT = 10;
+    public static final int EXPLICIT_WAIT = 20;
+
 
 }
